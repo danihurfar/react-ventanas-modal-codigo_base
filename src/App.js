0e0aa5a9f3f5239dcca293ade4593850a16eb38c
danihurfar <dahurfar@gmail.com>
2022-05-15 22:20:57 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react';
+import Modal from './componentes/Modal';
+import ContenidoPag from './componentes/ContenidoPag';
+import styled from 'styled-components';
+
+const App = () => {
+	const [estadoModal1, cambiarEstadoModal1] = useState(false);
+	return (
+		<div>
+			<ContenedorBotones>
+				<Boton onClick={() => cambiarEstadoModal1(!estadoModal1)}>Modal 1</Boton>
+			</ContenedorBotones>
+			<Modal
+			estado={estadoModal1}
+			cambiarEstado={cambiarEstadoModal1}
+			
+			mostrarHeader={true}
+			mostrarOverlay={true}
+			posicionModal={'center'}
+			>
+				<Contenido>
+					<h1> Ventana Modal</h1>
+					<p> Reutilizable</p>
+					<Boton onClick={() => cambiarEstadoModal1(!estadoModal1)}>Aceptar</Boton>
+				</Contenido>
+			</Modal>
+			<ContenidoPag>
+
+			</ContenidoPag>
+		</div>
+	);
+}
+ 
+export default App;
+
+const ContenedorBotones = styled.div`
+	padding: 40px;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	gap: 20px;
+`;
+
+const Boton = styled.button`
+	display: block;
+	padding: 10px 30px;
+	border-radius: 100px;
+	color: #fff;
+	border: none;
+	background: #1766DC;
+	cursor: pointer;
+	font-family: 'Roboto', sans-serif;
+	font-weight: 500;
+	transition: .3s ease all;
+
+	&:hover {
+		background: #0066FF;
+	}
+`;
+
+const Contenido = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	h1 {
+		font-size: 42px;
+		font-weight: 700;
+		margin-bottom: 10px;
+	}
+
+	p {
+		font-size: 18px;
+		margin-bottom: 20px;
+	}
+
+	img {
+		width: 100%;
+		vertical-align: top;
+		border-radius: 3px;
+	}
+`;
---
+++ src/componentes/ContenidoPag.js
@@ -0,0 +1,20 @@
+import React from 'react';
+class ContenidoPag extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            
+        }
+    }
+    render() { 
+        return ( 
+            <div className="card">
+            <div className="card-header">
+                Ejemplooo de la Pag. ppal
+            </div>
+             </div>
+         );
+    }
+}
+ 
+export default ContenidoPag ;
